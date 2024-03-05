9a42223e9c15575ef38a679dad6e306dec1b48b0
Kushan Yasiru <kushanyasiruwalpola@gmail.com>
2024-03-05 16:58:49 +0530

    Update api.service.ts

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,11 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { product } from '../component/product-view/productmodel';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+
+  public cartitemlist : any = [];
+  public productlist = new BehaviorSubject<any>([])
 
   constructor(private http:HttpClient) { }
 
@@ -15,5 +19,24 @@ export class ApiService {
 
   getproductbyid(id:string){
     return this.http.get("https://dummyjson.com/products/" +id)
+  }
+
+  addtocart(data: product){
+    this.cartitemlist.push(data);
+    this.productlist.next(this.cartitemlist);
+    console.log(this.cartitemlist)
+  }
+
+  products(){
+    return this.productlist.asObservable();
+  }
+
+  removecartitem(data: product){
+    this.cartitemlist.map((a:product, index:product)=>{
+      if(data.id ===a.id){
+        this.cartitemlist.splice(index, 1)
+      }
+    })
+    this.productlist.next(this.cartitemlist)
   }
 }
